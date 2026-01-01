8c8ee0a1ed7ddac6cdd2452f5f04131a49fde723
Jake Duncan <hello@spacefarm.dev>
2026-01-01 10:40:34 -0700

    Initial basic site

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home';
+import { ContactComponent } from './contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'alpal/contact', component: ContactComponent },
+];
