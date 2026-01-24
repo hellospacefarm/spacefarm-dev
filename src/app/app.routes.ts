89187e82f93c257a2cfdd9f9611cff717d68941d
Jake Duncan <hello@spacefarm.dev>
2026-01-24 10:40:53 -0700

    Add privacy policy page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home';
 import { ContactComponent } from './contact/contact';
+import { PrivacyComponent } from './privacy/privacy';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'alpal/contact', component: ContactComponent },
+  { path: 'alpal/privacy', component: PrivacyComponent },
 ];
---
+++ src/app/privacy/privacy.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-privacy',
+  standalone: true,
+  templateUrl: './privacy.html',
+  styleUrl: './privacy.css',
+})
+export class PrivacyComponent {}
